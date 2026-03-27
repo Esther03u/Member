67cb0df83948808f2e06af910d64a7a244f70da0
Esther03u <150509740+Esther03u@users.noreply.github.com>
2026-03-27 10:52:12 +0700

    Update project

--- connect1.php
+++ connect1.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        $link=mysqli_connect("localhost","root","","cs67");
-        if($link){
-            echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
-            mysqli_set_charset($link,"utf8");
-        }else{
-            echo "เชื่อมต่อฐานข้อมูลไม่สำเร็จ".mysqli_connect_error();
-            exit();
-        }
-    ?>
-</body>
-</html>
+<?php
+    $link = mysqli_connect("127.0.0.1", "root", "", "cs67", 3307);
+    if (!$link) {
+        die("เชื่อมต่อฐานข้อมูลไม่สำเร็จ: " . mysqli_connect_error());
+    }
+    mysqli_set_charset($link, "utf8");
+?>
